b3c2da1b9c311781c60f80fedbbc66e7e7a4e3a9
Anorqulov14 <eldoranorqulov@gmail.com>
2026-02-14 16:20:11 +0500

    todos

--- 4-dars/react/src/modules/index.jsx
+++
@@ -1,7 +0,0 @@
-import Header from './Header';
-import Hero from './Hero';
-import Category from './Category';
-import Trending from './Trending';
-import Footer from './Footer'
-
-export {Header, Hero, Category, Trending, Footer};
